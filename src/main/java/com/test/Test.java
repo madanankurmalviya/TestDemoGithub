02ae58f411f96056a83d0beb418be1de8d22cdc6
ANKUR MALVIYA <56480932+madanankurmalviya@users.noreply.github.com>
2024-01-04 06:46:50 -0800

    Update Test.java

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -3,5 +3,6 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello There...from local");
+        System.out.println("Hello There...from github");
     }
 }
